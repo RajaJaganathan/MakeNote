ea1967bf8be3804c996f6a02c53469074d65b17d
Raja J <raja_j@apple.com>
2017-03-27 22:16:43 +0530

    eslint fixed added

--- src/app/note/note.component.js
+++ src/app/note/note.component.js
@@ -1,8 +1,8 @@
-﻿import bootbox from 'bootbox';
+import bootbox from 'bootbox';
 
 export default {
-    bindings: {},
-    template: `<div class="stickynotes-container">
+  bindings: {},
+  template: `<div class="stickynotes-container">
                         <mn-note ng-repeat="note in $ctrl.notes | filter:$ctrl.searchText" 
                             note="note" 
                             ng-model="note" 
@@ -13,67 +13,68 @@ export default {
                             on-color-changer="$ctrl.onChangeColor(note)">
                         </mn-note>
                     </div>`,
-    controller: class NoteCtrl {
-        constructor($scope, $q, noteService) {
-            'ngInject';
-            this.$scope = $scope;
-            this.$q = $q;
-            this.noteService = noteService;
-        }
-
-        $onInit() {
-            this.note = {};
-            this.title = "Lorem ipsum";
-            this.content = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit";
-            this.category = "";
-            this.color = "#ffffcc";
-            this.colors = ["#ffffcc", "#d3fdb5" /*Green*/ , "#8fe5fe" /*blue*/ , "#ff6df1", "#c38af3"];
-            this.notes = [{}];
-            this.noteService.getNotes().then(data => {
-                this.notes = data;
-            });
-        }
-
-        addNote() {
-            var note = {
-                title: this.title,
-                content: this.content,
-                category: this.category,
-                color: this.color
-            };
-
-            this.notes.push(note);
-        }
-
-        saveNote(note) {
-            console.log("save Notes", note);
-        };
-
-        deleteNote(note) {
-            bootbox.dialog({
-                message: "Do you want to delete the note ?",
-                title: "Delete Note",
-                buttons: {
-                    danger: {
-                        label: "Delete!",
-                        className: "btn-danger",
-                        callback: () => {
-                            this.$scope.$apply(() => {
-                                this.notes.splice(this.notes.indexOf(note), 1);
-                            });
-                        }
-                    },
-                    main: {
-                        label: "Cancel!",
-                        className: "btn-primary"
-                    }
-                }
-            });
-        }
-
-        onChangeColor(note) {
-            var randomIndex = Math.random() * this.colors.length;
-            note.color = this.colors[parseInt(randomIndex)];
-        }
+  controller: class NoteCtrl {
+    constructor ($scope, $q, $log, noteService) {
+      'ngInject';
+      this.$scope = $scope;
+      this.$q = $q;
+      this.$log = $log;
+      this.noteService = noteService;
     }
-}
+
+    $onInit () {
+      this.note = {};
+      this.title = 'Lorem ipsum';
+      this.content = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit';
+      this.category = '';
+      this.color = '#ffffcc';
+      this.colors = ['#ffffcc', '#d3fdb5' /* Green */, '#8fe5fe' /* blue */, '#ff6df1', '#c38af3'];
+      this.notes = [{}];
+      this.noteService.getNotes().then(data => {
+        this.notes = data;
+      });
+    }
+
+    addNote () {
+      var note = {
+        title: this.title,
+        content: this.content,
+        category: this.category,
+        color: this.color
+      };
+
+      this.notes.push(note);
+    }
+
+    saveNote (note) {
+      this.$log.info('save Notes', note);
+    };
+
+    deleteNote (note) {
+      bootbox.dialog({
+        message: 'Do you want to delete the note ?',
+        title: 'Delete Note',
+        buttons: {
+          danger: {
+            label: 'Delete!',
+            className: 'btn-danger',
+            callback: () => {
+              this.$scope.$apply(() => {
+                this.notes.splice(this.notes.indexOf(note), 1);
+              });
+            }
+          },
+          main: {
+            label: 'Cancel!',
+            className: 'btn-primary'
+          }
+        }
+      });
+    }
+
+    onChangeColor (note) {
+      var randomIndex = Math.random() * this.colors.length;
+      note.color = this.colors[parseInt(randomIndex)];
+    }
+    }
+};
